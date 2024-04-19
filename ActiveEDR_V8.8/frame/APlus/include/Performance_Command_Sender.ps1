598b66365c2511f2d621ac1c360ec5137c30aa1e
Wan <yu.wan@aspentech.com>
2024-04-19 09:49:39 +0800

    Uploaded V15.0 ActivatedEDR

---
+++ ActiveEDR_V8.8/frame/APlus/include/Performance_Command_Sender.ps1
@@ -0,0 +1,12 @@
+﻿param($command,$target,$optional,$Testcase="test",$Step="",$Tag="",$Result="")
+
+$pipe = new-object System.IO.Pipes.NamedPipeClientStream("wwwPerfMonBase");
+$pipe.Connect();
+ 
+$sw = new-object System.IO.StreamWriter($pipe);
+$cmd=$command+"ý"+$target+"ý"+$optional+"ý"+$Testcase+"ý"+$Step+"ý"+$Tag+"ý"+$Result
+$sw.WriteLine($cmd);
+$sw.WriteLine('exit');
+ 
+$sw.Dispose();
+$pipe.Dispose()
